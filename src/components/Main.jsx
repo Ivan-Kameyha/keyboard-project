08129d44bb222d11ca12163c6d6a302e1fdd2f91
IvanKameyha <iv.kameyha@gmail.com>
2023-05-09 12:00:12 -0300

    Teclas funcionando

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,39 @@
 import React from "react";
-import "../styles/main.css"
+import "../styles/main.css";
 
 export function Main() {
   document.addEventListener("keydown", function (event) {
+    // Colorear teclas
+    const key = event.code;
+    const button = document.querySelector(`#${key}`);
+
+    if (button) {
+      button.classList.add('active');
+    }
+
+    // Scroll de la tecla "enter"
     if (event.keyCode === 13) {
       var elemento = document.getElementById("footer");
       elemento.scrollIntoView({ behavior: "smooth", block: "start" });
     }
+
+    // Redireccionar a la pagina deseada
     if (event.shiftKey && event.keyCode === 49) {
-      // redireccionar a la pagina deseada
       window.location.href = "http://localhost:5173/login";
     } else if (event.shiftKey && event.keyCode === 50) {
       window.location.href = "http://localhost:5173/register";
     }
   });
+
+  document.addEventListener("keyup", function(event) {
+    // Descolorear teclas
+    const key = event.code;
+    const button = document.querySelector(`#${key}`);
+
+    if (button) {
+      button.classList.remove('active');
+    }
+  })
 
   return (
     <div>
@@ -31,75 +51,75 @@ export function Main() {
 
       <div className="keyboard">
         <div className="row one">
-          <button>Esc</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>0</button>
-          <button>?</button>
-          <button>¿</button>
-          <button className="backspace">Backspace</button>
+          <button id="Escape">Esc</button>
+          <button id="Digit1">1</button>
+          <button id="Digit2">2</button>
+          <button id="Digit3">3</button>
+          <button id="Digit4">4</button>
+          <button id="Digit5">5</button>
+          <button id="Digit6">6</button>
+          <button id="Digit7">7</button>
+          <button id="Digit8">8</button>
+          <button id="Digit9">9</button>
+          <button id="Digit0">0</button>
+          <button id="Minus">?</button>
+          <button id="Equal">¿</button>
+          <button id="Backspace" className="backspace">Backspace</button>
         </div>
         <div className="row two">
-          <button className="tab">Tab</button>
-          <button>Q</button>
-          <button>W</button>
-          <button>E</button>
-          <button>R</button>
-          <button>T</button>
-          <button>Y</button>
-          <button>U</button>
-          <button>I</button>
-          <button>O</button>
-          <button>P</button>
-          <button>´</button>
-          <button className="otherKey">+</button>
-          <button className="enter">Enter</button>
+          <button id="Tab" className="tab">Tab</button>
+          <button id="KeyQ">Q</button>
+          <button id="KeyW">W</button>
+          <button id="KeyE">E</button>
+          <button id="KeyR">R</button>
+          <button id="KeyT">T</button>
+          <button id="KeyY">Y</button>
+          <button id="KeyU">U</button>
+          <button id="KeyI">I</button>
+          <button id="KeyO">O</button>
+          <button id="KeyP">P</button>
+          <button id="BracketLeft">´</button>
+          <button id="BracketRight" className="otherKey">+</button>
+          <button id="Enter" className="enter">Enter</button>
         </div>
         <div className="row three">
-          <button className="bloqMayus">Bloq Mayus</button>
-          <button>A</button>
-          <button>S</button>
-          <button>D</button>
-          <button>F</button>
-          <button>G</button>
-          <button>H</button>
-          <button>J</button>
-          <button>K</button>
-          <button>L</button>
-          <button>Ñ</button>
-          <button>&#123;</button>
-          <button>&#125;</button>
+          <button id="CapsLock" className="bloqMayus">Bloq Mayus</button>
+          <button id="KeyA">A</button>
+          <button id="KeyS">S</button>
+          <button id="KeyD">D</button>
+          <button id="KeyF">F</button>
+          <button id="KeyG">G</button>
+          <button id="KeyH">H</button>
+          <button id="KeyJ">J</button>
+          <button id="KeyK">K</button>
+          <button id="KeyL">L</button>
+          <button id="Semicolon">Ñ</button>
+          <button id="Quote">&#123;</button>
+          <button id="Backslash">&#125;</button>
         </div>
         <div className="row four">
-          <button className="shiftLeft">Shift</button>
-          <button>&lt;&gt;</button>
-          <button>Z</button>
-          <button>X</button>
-          <button>C</button>
-          <button>V</button>
-          <button>B</button>
-          <button>N</button>
-          <button>M</button>
-          <button>,</button>
-          <button>.</button>
-          <button>-</button>
-          <button className="shiftRight">Shift</button>
+          <button id="ShiftLeft" className="shiftLeft">Shift</button>
+          <button id="IntlBackslash">&lt;&gt;</button>
+          <button id="KeyZ">Z</button>
+          <button id="KeyX">X</button>
+          <button id="KeyC">C</button>
+          <button id="KeyV">V</button>
+          <button id="KeyB">B</button>
+          <button id="KeyN">N</button>
+          <button id="KeyM">M</button>
+          <button id="Comma">,</button>
+          <button id="Period">.</button>
+          <button id="Slash">-</button>
+          <button id="ShiftRight" className="shiftRight">Shift</button>
         </div>
         <div className="row five">
-          <button className="otherKey">Ctrl</button>
-          <button className="otherKey">Win</button>
-          <button className="otherKey">Alt</button>
-          <button className="space">Space</button>
-          <button className="otherKey">Alt</button>
-          <button className="otherKey">Win</button>
-          <button className="otherKey">Ctrl</button>
+          <button id="ControlLeft" className="otherKey">Ctrl</button>
+          <button id="MetaLeft" className="otherKey">Win</button>
+          <button id="AltLeft" className="otherKey">Alt</button>
+          <button id="Space" className="space">Space</button>
+          <button id="AltRight" className="otherKey">Alt</button>
+          <button id="MetaRight" className="otherKey">Win</button>
+          <button id="ControlRight" className="otherKey">Ctrl</button>
         </div>
       </div>
 
